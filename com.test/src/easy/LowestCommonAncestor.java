f4f4adbf2e8f1bca6d8afe3a8a1810b70e97a7e4
Chirag M Deekshith <chiragmoorthy1@gmail.com>
2022-05-13 18:56:05 +0530

    Tree problems

--- com.test/src/easy/LowestCommonAncestor.java
+++ com.test/src/easy/LowestCommonAncestor.java
@@ -1,4 +1,39 @@
 package easy;
 
+import common.TreeNode;
+
 public class LowestCommonAncestor {
+    public TreeNode lowestCommonAncestorRecursive(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestorRecursive(root.left, p, q);
+        }
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestorRecursive(root.right, p, q);
+        }
+        return root;
+    }
+
+    public TreeNode lowestCommonAncestorLoop(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode current = root;
+        while (true) {
+            if (p.val < current.val && q.val < current.val) {
+                current = current.left;
+            } else if (p.val > current.val && q.val > current.val) {
+                current = current.right;
+            } else {
+                return current;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+        //new LowestCommonAncestor().lowestCommonAncestor(root, new TreeNode(2), new TreeNode(4));
+    }
 }
