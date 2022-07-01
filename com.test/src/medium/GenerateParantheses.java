94cba4b23c9b08e614fadecce5eb9a15a8cb932d
Chirag M Deekshith <chiragmoorthy1@gmail.com>
2022-07-01 18:36:06 +0530

    generate parantheses

---
+++ com.test/src/medium/GenerateParantheses.java
@@ -0,0 +1,32 @@
+package medium;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class GenerateParantheses {
+    StringBuilder stringBuilder = new StringBuilder();
+    List<String> result = new ArrayList<>();
+    int n;
+    public List<String> generateParenthesis(int n) {
+        this.n = n;
+        dfs(0,0);
+        return result;
+    }
+
+    private void dfs(int open, int closed) {
+        if(stringBuilder.length() == n * 2 ) {
+            result.add(stringBuilder.toString());
+            return;
+        }
+        if(open < n) {
+            stringBuilder.append("(");
+            dfs(open + 1, closed);
+            stringBuilder.deleteCharAt(stringBuilder.length() -1);
+        }
+        if(closed < open) {
+            stringBuilder.append(")");
+            dfs(open, closed + 1);
+            stringBuilder.deleteCharAt(stringBuilder.length() -1);
+        }
+    }
+}
